2bd301472e2aea27a29173300f2c13efce3e5d64
Julio Seaman <jseaman76@gmail.com>
2021-04-15 23:24:20 +0200

    Implementing SDL

--- Wolf3DDemo.cpp
+++ Wolf3DDemo.cpp
@@ -1,14 +1,60 @@
 // Wolf3DDemo.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
 #include <iostream>
 #include <stdbool.h>
 
+#include <SDL.h>
+#undef main
+
+
+static SDL_Window* window = NULL;
+static SDL_Renderer* renderer = NULL;
+
 bool isGameRunning = true;
 
-void setup()
+bool initializeSDL()
 {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        std::cerr << "Error initializing SDL" << std::endl;
+        return false;
+    }
 
+    SDL_DisplayMode displayMode;
+    SDL_GetCurrentDisplayMode(0, &displayMode);
+    int fullScreenWidth = displayMode.w;
+    int fullScreenHeight = displayMode.h;
+
+    window = SDL_CreateWindow(
+        "Wolf3D Demo",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        fullScreenWidth,
+        fullScreenHeight,
+        SDL_WINDOW_OPENGL
+    );
+
+    if (!window)
+    {
+        std::cerr << "Error creating SDL window" << std::endl;
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    if (!renderer)
+    {
+        std::cerr << "Error creating SDL renderer" << std::endl;
+    }
+
+    return true;
+}
+
+bool setup()
+{
+    return initializeSDL();
 }
 
 void processInput()
@@ -23,17 +69,19 @@ void update()
 
 void render()
 {
-    std::cout << "Estoy jugando" << std::endl;
 }
 
 void releaseResources()
 {
-
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
 int main()
 {
-    setup();
+    if (!setup())
+        return 1;
 
     while (isGameRunning)
     {
