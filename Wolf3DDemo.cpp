d4e8b3665c5cb251cf6948e9470cbe1e7b01f1be
Julio Seaman <jseaman76@gmail.com>
2021-04-16 00:24:23 +0200

    Drawing map based on a matrix

--- Wolf3DDemo.cpp
+++ Wolf3DDemo.cpp
@@ -16,6 +16,29 @@ bool isGameRunning = true;
 int fullScreenWidth;
 int fullScreenHeight;
 
+#define TILE_SIZE 64
+
+#define MAP_NUM_ROWS 15
+#define MAP_NUM_COLS 16
+
+int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
 bool initializeSDL()
 {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
@@ -88,22 +111,27 @@ void render()
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
     SDL_Rect rect;
-    rect.x = 0;
-    rect.y = 0;
-    rect.w = 100;
-    rect.h = 100;
+    rect.w = TILE_SIZE;
+    rect.h = TILE_SIZE;
 
-    SDL_RenderFillRect(renderer, &rect);
+    for (int i = 0; i < MAP_NUM_ROWS; i++)
+        for (int j = 0; j < MAP_NUM_COLS; j++)
+        {
+            rect.x = j * TILE_SIZE;
+            rect.y = i * TILE_SIZE; 
 
-    rect.x = 200;
-    rect.y = 200;
+            if (map[i][j] == 0)
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            else 
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+            SDL_RenderFillRect(renderer, &rect);
 
-    SDL_RenderFillRect(renderer, &rect);
+            SDL_SetRenderDrawColor(renderer, 128, 0, 0, 255);
+            SDL_RenderDrawRect(renderer, &rect);
+        }
 
     SDL_RenderPresent(renderer);
 }
